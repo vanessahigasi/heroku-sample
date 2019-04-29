29fccf462bc3468cfccade21d3c8e1b88c4105a1
vanessahigasi <vanessahigasi@gmail.com>
2019-04-29 09:52:05 +0200

    Created Sample Api

---
+++ src/main/java/tech/bts/herokusample/api/SampleApi.java
@@ -0,0 +1,22 @@
+package tech.bts.herokusample.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api")
+
+public class SampleApi {
+
+    @RequestMapping(method = RequestMethod.GET,path = "/hello")
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from sample app";
+    }
+
+
+
+
+}
