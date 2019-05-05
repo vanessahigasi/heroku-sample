c6368c34524167c7edfbeda2c7c1f7ce4a3007bb
vanessahigasi <vanessahigasi@gmail.com>
2019-05-05 11:12:53 +0200

    exercise

--- src/main/java/tech/bts/herokusample/api/SampleApi.java
+++ src/main/java/tech/bts/herokusample/api/SampleApi.java
@@ -1,13 +1,34 @@
 package tech.bts.herokusample.api;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api")
-
 public class SampleApi {
+
+    private final MongoCollection<Document> words;
+
+    @Autowired
+    public SampleApi(@Value("${mongoUri}") String mongoUri) {
+
+        final MongoClient mongoClient = MongoClients.create(mongoUri);
+        final MongoDatabase database = mongoClient.getDatabase("test");
+        this.words = database.getCollection("words");
+    }
 
     //@RequestMapping(method = RequestMethod.GET,path = "/hello")
     @GetMapping("/hello")
@@ -15,7 +36,30 @@ public class SampleApi {
         return "Hello from sample app";
     }
 
+    // insert?word=dog
+    @GetMapping("/insert")
+    public String insertWord(@RequestParam String word) {
 
+        Document doc = new Document()
+                .append("word", word)
+                .append("date", new Date());
+
+        words.insertOne(doc);
+        return "Word inserted: " + word;
+    }
+
+    // insert?word=dog
+    @GetMapping("/list")
+    public List<String> listWords() {
+
+        final List<String> result = new ArrayList<>();
+
+        for (Document doc : words.find()) {
+            result.add(doc.getString("word"));
+        }
+
+        return result;
+    }
 
 
 }
